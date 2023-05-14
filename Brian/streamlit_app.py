758efda6ed6c74bcdb6ca08fdd8acd5d80d8bf07
brianwander <brianwander101@gmail.com>
2023-05-14 17:36:34 -0400

    Added functionality to mint NFTs from uploaded artworks

--- Brian/streamlit_app.py
+++ Brian/streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 from thirdweb import ThirdwebSDK
+from dotenv import load_dotenv
+import os
+from functions import mint_nft
 
 #Names for the different tabs of the streamlit application
 tab1,tab2,tab3,tab4 = st.tabs(["Welcome","Mint","Browse","Trade"])
@@ -7,7 +10,16 @@ tab1,tab2,tab3,tab4 = st.tabs(["Welcome","Mint","Browse","Trade"])
 with tab1:
     st.title("NFT Marketplace")#Title
     st.header("Mint NFTs from your original artwork, and buy and sell them on the Ethereum Blockchain")#Explanation of the application
-    st.write("Head over to the Mint tab to upload your work, or the Trade screen to buy and sell NFTs")#How to use the application
+    st.write("First, input the contract address below. Then, head over to the Mint tab to upload your work, or the Trade screen to buy and sell NFTs")#How to use the application
+    contract_address=st.text_input("NFT Contract address")
+    if st.button("Load Contract"):
+        load_dotenv()
+        private_key=os.getenv("mumbai_private_key")
+        sdk= ThirdwebSDK.from_private_key(private_key,"mumbai")
+        contract = sdk.get_nft_collection(str(contract_address))
+        st.write(contract)
+
+
 #In the Mint tab:
 with tab2:
     st.warning("By using this service you agree that the work you are uploading is your own, original artwork.",icon="⛔️")#Displays a warning message to the user
@@ -15,6 +27,14 @@ with tab2:
     if st.button("Display Artwork"): #A button that displays the uploaded image
         st.image(artwork)
     address=st.text_input("Address to which the NFT will be minted") #The address that will own the NFT
+    nft_name=st.text_input("Give a name to your NFT")
+    nft_description=st.text_input("Give a description to your NFT")
+    if st.button("Mint NFT"):
+        load_dotenv()
+        private_key=os.getenv("mumbai_private_key")
+        sdk= ThirdwebSDK.from_private_key(private_key,"mumbai")
+        contract = sdk.get_nft_collection(str(contract_address))
+        mint_nft(contract_object=contract,name=nft_name,description=nft_description,image=artwork,address=address)
 #contract.call("function_name", "arg1", "arg2") #Pseudocode for functions
 #contract.call("mintto",artwork) #Pseudocode for minting
 
